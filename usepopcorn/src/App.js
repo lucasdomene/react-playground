dd28b5466625eb18b57cbf198aae50f37d7e6921
Lucas Domene <lucas.firmo@gmail.com>
2025-01-27 09:31:55 -0300

    feat[usepopcorn]: loading indicator

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar';
 import MovieList from './components/MovieList';
 import Search from './components/Search';
 import MovieResultCount from './components/MovieResultCount';
-import { tempMovieData, tempWatchedData } from './constants';
+
 import Box from './components/Box';
 import WatchedSummary from './components/WatchedSummary';
 import WatchedMovieList from './components/WatchedMovieList';
@@ -14,12 +14,21 @@ const KEY = process.env.REACT_APP_OMDB_API_KEY;
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('interstellar');
 
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
-  }, []);
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    };
+    fetchMovies();
+  }, [query]);
 
   return (
     <>
@@ -29,6 +38,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
+          {isLoading ? <Loader /> : <MovieList movies={movies} />}
           <MovieList movies={movies} />
         </Box>
         <Box>
@@ -38,4 +48,8 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
